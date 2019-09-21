37573cafef632ace396bae9ecbe21d3c3ecb6bc9
Nnphbe <49986257+Nnphbe@users.noreply.github.com>
2019-09-22 01:04:31 +0700

    Add files via upload

--- WordCard.js
+++ WordCard.js
@@ -33,6 +33,7 @@ export default class WordCard extends Component {
     render(){
         return (
             <div>
+                <p>ROUND : {this.state.attempt}</p>
                 {Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler} attempt={this.state.attempt}/>) }
             </div>
         );
